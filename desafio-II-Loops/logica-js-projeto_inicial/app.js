54d7a5ba647969c5e6b6749a44f83ef304566377
Larissa Rodrigues <larissarodrigues1599@gmail.com>
2025-02-10 23:33:09 -0300

    Treinando laços de repetição

---
+++ desafio-II-Loops/logica-js-projeto_inicial/app.js
@@ -0,0 +1,24 @@
+let contador = 1;
+let contadorII = 10;
+let contadorIII = 0;
+let numeroMinimo = prompt("Digite um número para a contagem regressiva: ");
+let numeroMaximo = prompt("Digite um número para a contagem progressiva: ");
+
+while(contador <= 10){
+
+    console.log(contador); 
+    contador++;
+
+} 
+ while (contadorII >= 0){
+    console.log(contadorII);
+    contadorII--;
+ }
+ while(numeroMinimo >= 0){
+    console.log(numeroMinimo);
+    numeroMinimo--;
+ }
+ while(contadorIII <= numeroMaximo){
+    console.log(numeroMaximo);
+    contadorIII++;
+}
